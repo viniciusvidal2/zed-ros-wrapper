f2cf46cf9529d8ad15c22b335db20e6a49f79861
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2022-01-27 13:39:56 -0500

    updated for better data acquisition

--- zed_nodelets/src/zed_nodelet/include/zed_wrapper_nodelet.hpp
+++ zed_nodelets/src/zed_nodelet/include/zed_wrapper_nodelet.hpp
@@ -415,7 +415,7 @@ private:
   bool mStopNode = false;
 
   // Vinicius - reduce IMU rate for FAST-LIO
-  const double mSensPubRate = 100.0; // Maximum ODR for ZED2/ZED2i. You can change this to 800 for ZED-M, but it's not recommended
+  const double mSensPubRate = 200.0; // Maximum ODR for ZED2/ZED2i. You can change this to 800 for ZED-M, but it's not recommended
 
   // Publishers
   image_transport::CameraPublisher mPubRgb;       //
